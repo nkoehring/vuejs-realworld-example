a8f1d21a63d0304690285e49d0fc812120c16bf8
koehr <n@koehr.in>
2018-08-27 23:03:32 +0200

    fixes favorite counts and article preview (tags)

--- src/components/ArticleMeta.vue
+++ src/components/ArticleMeta.vue
@@ -13,20 +13,21 @@
 
   <template v-if="preview">
     <button class="btn btn-outline-primary btn-sm pull-xs-right">
-      <i class="ion-heart"></i> {{ article.favouritesCount }}
+      <i class="ion-heart"></i> {{ article.favoritesCount }}
     </button>
   </template>
   <template v-else>
     <button class="btn btn-sm btn-outline-secondary">
       <i class="ion-plus-round"></i>
       &nbsp;
-      Follow Eric Simons <span class="counter">({{ article.author.followersCount }})</span>
+      Follow {{ article.author.username }}
     </button>
     &nbsp;&nbsp;
     <button class="btn btn-sm btn-outline-primary">
       <i class="ion-heart"></i>
       &nbsp;
-      Favorite Post <span class="counter">({{ article.favouritesCount }})</span>
+      Favorite Post
+      <span class="counter">({{ article.favoritesCount }})</span>
     </button>
   </template>
 </div>
--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -5,6 +5,13 @@
     <h1>{{ article.title }}</h1>
     <p>{{ article.description }}</p>
     <span>Read more...</span>
+    <ul class="tag-list">
+      <li v-for="tag in article.tagList"
+        :key="tag"
+        class="tag-default tag-pill tag-outline">
+        {{ tag }}
+      </li>
+    </ul>
   </router-link>
 </div>
 </template>
